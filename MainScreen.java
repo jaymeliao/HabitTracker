fc131680b81b34bc04c7199bae8e34a5a7df54d8
jaymeliao <34082315+jaymeliao@users.noreply.github.com>
2018-07-09 09:03:47 +0800

    Create MainScreen.java

---
+++ MainScreen.java
@@ -0,0 +1,16 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class MainScreen extends JFrame {
+	
+	public static void main(String args[])
+	{
+		@SuppressWarnings("unused")
+		Habit h = new Habit();	
+	}
+
+}
